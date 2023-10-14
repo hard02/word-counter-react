0d5b0fda731d7b63e395800fea33e8b5b52637f7
Hardik Kankane <98350448+hard02@users.noreply.github.com>
2023-10-14 16:56:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WordCounter from './WordCounter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Responsive Word Counter</h1>
+      <WordCounter />
+    </div>
+  );
+}
+
+export default App;
+
